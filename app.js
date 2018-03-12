47fab9ff1d1320e06a930a501a3e53ea3458b4ae
aidemar <aidemarmag@hotmail.com>
2018-03-12 13:25:19 -0600

    iniciando

---
+++ app.js
@@ -0,0 +1,71 @@
+var requirejs = require('requirejs');
+
+requirejs.config({
+    "baseUrl": "index.html",
+    "paths": {
+      "app": "../app",
+      "jquery": "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min"
+    }
+});
+
+// Load the main app module to start the app
+requirejs(["app"]);
+
+requirejs.config({
+    //Pass the top-level main.js/index.js require
+    //function to requirejs so that node modules
+    //are loaded relative to the top-level JS file.
+    nodeRequire: require
+});
+
+requirejs(['foo', 'bar'],
+function   (foo,   bar) {
+    //foo and bar are loaded according to requirejs
+    //config, but if not found, then node's require
+    //is used to load the module.
+});
+
+var conekta = require('conekta');
+
+conekta.api_key = 'CtTiutDz9Zd6Wbqs5S9xUGw';
+conekta.locale = 'es';
+
+
+// conekta.setPublicKey("key_CtTiutDz9Zd6Wbqs5S9xUGw");
+
+Conekta.getPublicKey();
+
+Conekta.setLanguage("es");          // "en" para inglés
+
+Conekta.getLanguage();
+
+var successResponseHandler = function(token) {
+  // Do something on sucess
+  // you need to send the token to the backend.
+};
+
+
+
+
+ var conektaSuccessResponseHandler = function(token) {
+   var $form = $("#card-form");
+   //Inserta el token_id en la forma para que se envíe al servidor
+    $form.append($('<input type="hidden" name="conektaTokenId" id="conektaTokenId">').val(token.id));
+   $form.get(0).submit(); //Hace submit
+ };
+ var conektaErrorResponseHandler = function(response) {
+   var $form = $("#card-form");
+   $form.find(".card-errors").text(response.message_to_purchaser);
+   $form.find("button").prop("disabled", false);
+ };
+
+ //jQuery para que genere el token después de dar click en submit
+ $(function () {
+   $("#card-form").submit(function(event) {
+     var $form = $(this);
+     // Previene hacer submit más de una vez
+     $form.find("button").prop("disabled", true);
+     Conekta.Token.create($form, conektaSuccessResponseHandler, conektaErrorResponseHandler);
+     return false;
+   });
+ });
